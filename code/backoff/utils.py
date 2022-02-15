cfd6fca77103f6aedfcc778f7d7cb617ff3c1951
Vetle Hofsøy-Woie <vho023@uit.no>
2022-02-15 16:00:20 +0100

    Fixed backoff to python3

--- code/backoff/utils.py
+++ code/backoff/utils.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import unittest
 
 WINDOW = 5               # Number of slots that can have proposals pending
 TIMEOUTMULTIPLY = 1.2    # Multiplicative increase amount for liveness timeouts
@@ -12,6 +13,11 @@ class BallotNumber(namedtuple('BallotNumber',['round','leader_id'])):
     __slots__ = ()
     def __str__(self):
         return "BN(%d,%s)" % (self.round, str(self.leader_id))
+    def __gt__(self, other):
+        if other is None:
+            return True
+        else:
+            return super().__gt__(other)
 
 class PValue(namedtuple('PValue',['ballot_number','slot_number','command'])):
     """
@@ -59,3 +65,19 @@ class Config(namedtuple('Config',['replicas','acceptors','leaders'])):
                              ','.join(self.acceptors),
                              ','.join(self.leaders))
 
+
+
+class test_ballot_number(unittest.TestCase):
+  def setUp(self):
+    self.x = BallotNumber(2,1)
+    self.y = BallotNumber(3,0)
+
+  def test_compare_none(self):
+    self.assertTrue(self.x > None)
+  def test_compare_larger(self):
+    self.assertTrue(self.y > self.x)
+  def test_comapre_smaller(self):
+    self.assertFalse(self.x > self.y)
+    
+if __name__ == "__main__":
+  unittest.main()
