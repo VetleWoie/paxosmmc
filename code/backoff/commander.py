51a77c35886cbe5d2f0d276650cfa94679916345
Vetle Hofsøy-Woie <vho023@uit.no>
2022-02-20 17:33:54 +0100

    Commander might work

--- code/backoff/commander.py
+++ code/backoff/commander.py
@@ -1,5 +1,6 @@
+from httpprocess import Process
 from message import P2aMessage,P2bMessage,PreemptedMessage,DecisionMessage
-from process import Process
+# from process import Process
 from utils import Command
 
 class Commander(Process):
@@ -8,16 +9,20 @@ class Commander(Process):
     protocol.  Every commander is created for a specific ballot
     number, slot number and command triple.
     """
-    def __init__(self, env, id, leader, acceptors, replicas,
-                             ballot_number, slot_number, command):
-        Process.__init__(self, env, id)
+    def __init__(self,server_address, handler_class,id, leader, acceptors, replicas,
+                             ballot_number, slot_number, command, port_handler):
         self.leader = leader
         self.acceptors = acceptors
         self.replicas = replicas
         self.ballot_number = ballot_number
         self.slot_number = slot_number
         self.command = command
-        self.env.addProc(self)
+        self.port_handler = port_handler
+        Process.__init__(self,server_address, handler_class, id)
+
+    def shutdown(self) -> None:
+        super().shutdown()
+        self.port_handler.put(self.server_address[1])
 
     def body(self):
         """
@@ -52,9 +57,26 @@ class Commander(Process):
                     if len(waitfor) < float(len(self.acceptors))/2:
                         for r in self.replicas:
                             self.sendMessage(r, DecisionMessage(self.id, self.slot_number, self.command))
-                        return
+                        #Shutdown server when job is finished
+                        return self.shutdown()
                 else:
                     self.sendMessage(self.leader, PreemptedMessage(self.id, msg.ballot_number))
-                    return
+                    #Shutdown server when job is finished
+                    return self.shutdown()
 
 
+if __name__ == "__main__":
+
+    import multiprocessing
+    from httpprocess import Handler
+    import sys
+    if len(sys.argv) < 3:
+        print(f"Usage: {sys.argv[0]} [adress] [configfile]")
+        exit(0)
+
+    adress = sys.argv[1].split(':')
+    port_handler = multiprocessing.Manager().Queue()
+    scout = Commander((adress[0], int(adress[1])), Handler, sys.argv[1], None, None, None,None,None,None,port_handler,)
+    port = port_handler.get()
+    print(port)
+    
